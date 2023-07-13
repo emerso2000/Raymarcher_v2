18a0881ca0f12c7c9827c5ad630cea73cb9e59a8
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-07-12 22:14:47 -0400

    Update Schwarzschild.glsl

--- Schwarzschild.glsl
+++ Schwarzschild.glsl
@@ -227,55 +227,6 @@ vec3 marchRay(vec4 origin, vec4 direction) {
     return vec3(0.115, 0.133, 0.173);
 }
 
-vec4 get_timelike_vector(vec3 cartesian_basis_speed, float time_direction, float e0, float e1, float e2, float e3) {
-    float v = length(cartesian_basis_speed);
-    float Y = 1.0 / sqrt(1.0 - v * v);
-
-    float B = v;
-
-    float psi = B * Y;
-
-    vec4 e0vec = vec4(e0, 0, 0, 0);
-    vec4 e1vec = vec4(0, e1, 0, 0);
-    vec4 e2vec = vec4(0, 0, e2, 0);
-    vec4 e3vec = vec4(0, 0, 0, e3);
-
-    vec4 bT = time_direction * Y * e0vec;
-
-    vec3 dir = normalize(cartesian_basis_speed);
-
-    if (v == 0.0) {
-        dir = vec3(0.0, 0.0, 1.0);
-    }
-
-    vec4 bX = psi * dir.x * e1vec;
-    vec4 bY = psi * dir.y * e2vec;
-    vec4 bZ = psi * dir.z * e3vec;
-
-    return vec4(bT + bX + bY + bZ);
-}
-
-vec4 integrateGeodesic(vec4 currentPosition, vec4 currentVelocity) {
-    float timeStep = 0.01;
-
-    vec4 acceleration = vec4(0.0, 0.0, 0.0, 0.0);
-
-    mat4 christoffelSymbols_alpha_r = calculateChristoffelSymbolsAlphaR(currentPosition);
-    mat4 christoffelSymbols_alpha_theta = calculateChristoffelSymbolsAlphaTheta(currentPosition);
-    mat4 christoffelSymbols_alpha_phi = calculateChristoffelSymbolsAlphaPhi(currentPosition);
-    mat4 christoffelSymbols_alpha_time = calculateChristoffelSymbolsAlphaTime(currentPosition);
-
-    // Calculate the accelerations using the geodesic equation
-    acceleration.x = -dot(currentVelocity.xyzw, christoffelSymbols_alpha_r * currentVelocity.xyzw);
-    acceleration.y = -dot(currentVelocity.xyzw, christoffelSymbols_alpha_theta * currentVelocity.xyzw);
-    acceleration.z = -dot(currentVelocity.xyzw, christoffelSymbols_alpha_phi * currentVelocity.xyzw);
-    acceleration.w = -dot(currentVelocity.xyzw, christoffelSymbols_alpha_time * currentVelocity.xyzw);
-
-    currentVelocity.xyzw += acceleration * timeStep;
-
-    return currentVelocity.xyzw;
-}
-
 void main() {
     vec4 pixel = vec4(0.115, 0.133, 0.173, 1.0);
     ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
@@ -311,13 +262,6 @@ void main() {
     );    
     
     ellisRd = ellisRd * tetradMatrix; //transform to true schwarzschild coordinates
-
-    vec4 initialPosition = vec4(0, camera.cam_o);
-    vec4 initialVelocity = get_timelike_vector(vec3(0, 0, 1), 1.0, e0, e1, e2, e3);
-
-    // for (int i = 0; i < MAX_STEPS; i++) {
-    //     camera.cam_o = integrateGeodesic(initialPosition, initialVelocity).xyz;
-    // }
 
     vec3 color = marchRay(ellisRo, ellisRd);
 
